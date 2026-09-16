9a3fdf4c7d0f7a78b6a54429b9e3f57d4e35f855
Aditya Garg <adigarg4083@gmail.com>
2026-09-16 10:46:56 -0800

    fix scrolling of the sign-in page

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 
 
-const AuthLayout = ({children}:{children:React.ReactNode})=>{
-    return(
-        <main className="flex justify-center items-center h-screen flex-col bg-zinc-900">
-        {children}
-        </main>
-    )
-}
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center py-10 bg-zinc-900">
+      {children}
+    </div>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
+
